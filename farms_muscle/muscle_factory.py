26516f18e23b65411dfdc938b910045e993d2572
ShravanTata <tatarama@biorobpc14.epfl.ch>
2019-03-15 17:06:18 +0100

    Added muscle factory class

---
+++ farms_muscle/muscle_factory.py
@@ -0,0 +1,49 @@
+]"""Factory class for generating the muscle model."""
+
+from .geyer_muscle import GeyerMuscle
+
+class MuscleFactory(object):
+    """Implementation of Factory Muscle class.
+    """
+
+    def __init__(self):
+        """Factory initialization."""
+        super(MuscleFactory, self).__init__()
+        self._neurons = {'geyer': GeyerMuscle,}
+
+    def register_muscle(self, muscle_type, muscle_instance):
+        """
+        Register a new type of muscle that is a child class of Neuron.
+        Parameters
+        ----------
+        self: type
+            description
+        muscle_type: <str>
+            String to identifier for the muscle.
+        muscle_instance: <cls>
+            Class of the muscle to register.
+        """
+        self._muscles[muscle_type] = muscle_instance
+
+    def gen_muscle(self, muscle_type):
+        """Generate the necessary type of muscle.
+        Parameters
+        ----------
+        self: type
+            description
+        muscle_type: <str>
+            One of the following list of available muscles.
+            1. geyer - Geyer Muscle
+        
+        Returns
+        -------
+        muscle: <cls>
+            Appropriate muscle class.
+        """
+        muscle = self._muscles.get(muscle_type)
+        if not muscle:
+            raise ValueError(muscle_type)
+        return muscle
+
+
+
