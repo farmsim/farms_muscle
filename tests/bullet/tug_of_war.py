21d2981a6bc77fc11a3f7a312bb4ce026ecb3e05
ShravanTata <tatarama@biorobpc14.epfl.ch>
2019-09-19 14:42:29 +0200

    Update tug_of_war bullet test module
    Added the generation of the model. No muscles yet!

--- tests/bullet/tug_of_war.py
+++ tests/bullet/tug_of_war.py
@@ -39,47 +39,60 @@ plane = p.loadURDF("plane.urdf", [0, 0, 0], globalScaling=1)
 
 ########## LINKS ##########
 
-# visBoxId = p.createVisualShape(
-#     p.GEOM_BOX,
-#     visualFramePosition=[0,0,-0.5],
-#     visualFrameOrientation=[0,0,0,1],
-#     halfExtents=[0.05, 0.05, 0.5])
+vis_static_block = p.createVisualShapeArray(shapeTypes=[p.GEOM_BOX, p.GEOM_BOX],
+                                            halfExtents=[
+                                                [0.5, 0.1, 0.1], [0.5, 0.1, 0.1]],
+                                            visualFramePositions=[
+                                                [0., -0.35, 0.], [0., 0.35, 0.]])
 
-length = 0.5
-mass = 0.
-link_mass = 1
-visualShapeId = -1
+col_static_block = p.createCollisionShapeArray(shapeTypes=[p.GEOM_BOX, p.GEOM_BOX],
+                                            halfExtents=[
+                                                [0.5, 0.1, 0.1], [0.5, 0.1, 0.1]],
+                                            collisionFramePositions=[
+                                                [0., -0.35, 0.], [0., 0.35, 0.]])
 
-num_links = 4
-basePosition = [0, 0,2*length+num_links]
-baseOrientation = [0, 0, 0, 1]
-baseColId = p.createCollisionShape(p.GEOM_CAPSULE,
-                                   height=length,
-                                   radius=length*0.05,
-                                   collisionFramePosition=[0, 0, -length*0.5])
-link_Masses = [link_mass for j in range(num_links)]
-linkCollisionShapeIndices = [p.createCollisionShape(p.GEOM_CAPSULE,
-                                                    height=length,
-                                                    radius=length*0.05,
-                                                    collisionFramePosition=[
-                                                        0, 0, -length*0.5]
-                                                    ) for j in range(num_links)]
-# linkVisualShapeIndices = [visBoxId]
-linkVisualShapeIndices = [-1 for j in range(num_links)]
-linkPositions = [[0, 0, -length] for j in range(num_links)]
-linkOrientations = [[0.0, 0, 0, 1] for j in range(num_links)]
-linkInertialFramePositions = [
-    [0, 0, -length*0.5*(j+1)] for j in range(num_links)]
-linkInertialFrameOrientations = [[0, 0, 0, 1] for j in range(num_links)]
-indices = [j for j in range(num_links)]
-jointTypes = [p.JOINT_REVOLUTE for j in range(num_links)]
-axis = [[1, 0, 0] for j in range(num_links)]
+vis_moving_block = p.createVisualShape(
+    p.GEOM_BOX,
+    visualFramePosition=[0,0,0.],
+    visualFrameOrientation=[0,0,0,1],
+    halfExtents=[0.1, 0.1, 0.1])
+
+col_moving_block = p.createCollisionShape(
+    p.GEOM_BOX,
+    collisionFramePosition=[0,0,0.],
+    collisionFrameOrientation=[0,0,0,1],
+    halfExtents=[0.1, 0.1, 0.1])
+
+base_mass = 0. #: Static
+base_position = [0., 0., 0.1]
+base_orientation = [1., 0., 0., 0.]
+
+#: Moving block
+mass = 20.
+position = [0., 0., 0.]
+orientation = p.getQuaternionFromEuler([0., 0., 0.])
+
+system = p.createMultiBody(
+    base_mass, col_static_block, vis_static_block, base_position, base_orientation,
+    linkMasses=[mass], linkCollisionShapeIndices=[col_moving_block,],
+    linkVisualShapeIndices=[vis_moving_block,],
+    linkPositions=[position,], linkOrientations=[orientation],
+    linkInertialFramePositions=[position,],
+    linkInertialFrameOrientations=[orientation],
+    linkParentIndices=[0], linkJointTypes=[p.JOINT_PRISMATIC],
+    linkJointAxis=[[0., 1., 0.]])
+
+p.changeDynamics(system, 0, lateralFriction=0.0,
+                 localInertiaDiagonal=[[0.1333, 0.1333, 0.1333]])
+
+rendering(1)
 
 #: RUN
-for j in range(N):
+RUN = True
+while RUN:
     keys = p.getKeyboardEvents()
     if keys.get(113):
+        RUN=False
         break
-
     time.sleep(0.001)
     p.stepSimulation()
