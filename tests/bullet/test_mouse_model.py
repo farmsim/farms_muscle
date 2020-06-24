1c2025bb32b24d11ee99dfdbced633cd31ffd456
Shravan Tata Ramalingasetty <tatarama@>
2020-06-24 17:03:00 +0200

    [TEST] Updated mouse script

--- tests/bullet/test_mouse_model.py
+++ tests/bullet/test_mouse_model.py
@@ -1,6 +1,8 @@
 """ Test mouse model in pybullet. """
 
-from farms_models.utils import get_farms_model_path
+from farms_models.utils import (
+    get_farms_model_path, get_sdf_path, get_model_path
+)
 from animals.generic.bullet_simulation import BulletSimulation
 from IPython import embed
 import pybullet as p
@@ -13,6 +15,7 @@ import numpy as np
 import os
 from farms_sdf.units import SimulationUnitScaling
 from farms_container import Container
+import matplotlib.pyplot as plt
 pylog.set_level('error')
 
 
@@ -24,23 +27,52 @@ class MouseSimulation(BulletSimulation):
         super(MouseSimulation, self).__init__(
             container, SimulationUnitScaling(), **sim_options
         )
-        u = container.muscles.activations
-        self.muscle_params = {}
-        self.muscle_excitation = {}
-        for muscle in self.muscles.muscles.keys():
-            self.muscle_params[muscle] = u.get_parameter(
-                'stim_{}'.format(muscle)
-            )
-            self.muscle_excitation[muscle] = p.addUserDebugParameter(
-                "flexor {}".format(muscle), 0, 1, 0.05
-            )
+        self.connection_mode = p.getConnectionInfo(
+            self.physics_id
+        )['connectionMethod']
+        if self.MUSCLES and (self.connection_mode == 1):
+            u = container.muscles.activations
+            self.muscle_params = {}
+            self.muscle_excitation = {}
+            for muscle in self.muscles.muscles.keys():
+                self.muscle_params[muscle] = u.get_parameter(
+                    'stim_{}'.format(muscle)
+                )
+                self.muscle_excitation[muscle] = p.addUserDebugParameter(
+                    "flexor {}".format(muscle), 0, 1, 0.05
+                )
 
     def controller_to_actuator(self):
         """ Implementation of abstractmethod. """
-        for muscle in self.muscles.muscles.keys():
-            self.muscle_params[muscle].value = p.readUserDebugParameter(
-                self.muscle_excitation[muscle]
-            )
+        if self.MUSCLES and (self.connection_mode == 1):
+            for muscle in self.muscles.muscles.keys():
+                self.muscle_params[muscle].value = p.readUserDebugParameter(
+                    self.muscle_excitation[muscle]
+                )
+        # p.setJointMotorControl2(
+        #     self.animal,
+        #     self.joint_id['LHip'],
+        #     p.POSITION_CONTROL,
+        #     targetPosition=np.sin(2*np.pi*0.5*self.TIME)*0.8
+        # )
+        # p.setJointMotorControl2(
+        #     self.animal,
+        #     self.joint_id['RHip'],
+        #     p.POSITION_CONTROL,
+        #     targetPosition=np.sin(2*np.pi*0.5*self.TIME)*0.8
+        # )
+        # p.setJointMotorControl2(
+        #     self.animal,
+        #     self.joint_id['LKnee'],
+        #     p.POSITION_CONTROL,
+        #     targetPosition=0.0
+        # )
+        # p.setJointMotorControl2(
+        #     self.animal,
+        #     self.joint_id['RKnee'],
+        #     p.POSITION_CONTROL,
+        #     targetPosition=0.0
+        # )
 
     def feedback_to_controller(self):
         """ Implementation of abstractmethod. """
@@ -59,27 +91,27 @@ def main():
     """ Main """
 
     sim_options = {"headless": False,
-                   "model": os.path.join(
-                       get_farms_model_path(),
-                       "mouse_v1/design/sdf/mouse_locomotion.sdf"
-                   ),
+                   "model": get_sdf_path("mouse", "3"),
                    "model_offset": [0., 0., 0.1],
-                   "run_time": 10.,
+                   "run_time": 45.,
                    "planar": False,
                    "muscles": os.path.join(
-                       get_farms_model_path(),
-                       "mouse_v1/config/hind_muscles.yaml"
+                       get_model_path("mouse", "3"),
+                       "config/hind_locomotion_muscles_scaled.yaml"
                    ),
-                   "track": False,
-                   "slow_down": True,
+                   "track": True,
+                   "camera_yaw": 270,
+                   "slow_down": False,
                    "sleep_time": 0.001,
                    "base_link": "Pelvis"
                    }
-    container = Container(max_iterations=int(10/0.001))
+    container = Container(
+        max_iterations=int(sim_options['run_time']/0.001
+        )
+    )
     animal = MouseSimulation(container, sim_options)
     animal.run()
-    continer.dump(overwrite=True)
-
+    container.dump(overwrite=True)
 
 if __name__ == '__main__':
     main()
