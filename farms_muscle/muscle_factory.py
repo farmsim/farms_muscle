730515a6c31afcc64103f33800c9f1ce212d7583
shravantata <shravantr@gmail.com>
2023-01-27 14:12:53 -0500

    [FACTORY] Added simpler muscle model enum for factory class

--- farms_muscle/muscle_factory.py
+++ farms_muscle/muscle_factory.py
@@ -1,53 +1,8 @@
-"""Factory class for generating the muscle model."""
+"""Class for generating the muscle model."""
 
-from farms_muscle.geyer_muscle import GeyerMuscle
-from farms_muscle.millard_rigid_tendon_muscle import MillardRigidTendonMuscle
-from farms_muscle.millard_damped_equillibrium_muscle import MillardDampedEquillibriumMuscle
-from farms_muscle.degroote_muscle import DeGrooteMuscle
-
-
-class MuscleFactory(object):
-    """Implementation of Factory Muscle class.
-    """
-
-    def __init__(self):
-        """Factory initialization."""
-        super(MuscleFactory, self).__init__()
-        self._muscles = {'geyer': GeyerMuscle,
-                         'millard_de': MillardDampedEquillibriumMuscle,
-                         'millard_rt': MillardRigidTendonMuscle,
-                         'degroote': DeGrooteMuscle}
-
-    def register_muscle(self, muscle_type, muscle_instance):
-        """
-        Register a new type of muscle that is a child class of Neuron.
-        Parameters
-        ----------
-        self: type
-            description
-        muscle_type: <str>
-            String to identifier for the muscle.
-        muscle_instance: <cls>
-            Class of the muscle to register.
-        """
-        self._muscles[muscle_type] = muscle_instance
-
-    def gen_muscle(self, muscle_type):
-        """Generate the necessary type of muscle.
-        Parameters
-        ----------
-        self: type
-            description
-        muscle_type: <str>
-            One of the following list of available muscles.
-            1. geyer - Geyer Muscle
-
-        Returns
-        -------
-        muscle: <cls>
-            Appropriate muscle class.
-        """
-        muscle = self._muscles.get(muscle_type.lower())
-        if not muscle:
-            raise ValueError(muscle_type)
-        return muscle
+class MuscleModel(IntEnum):
+    """ Different muscle model implementations  """
+    MILLARDRT = 0
+    DEGROOTE = 1
+    GEYER = 2
+    BROWN = 3
